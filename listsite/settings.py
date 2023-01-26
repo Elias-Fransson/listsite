1e26c678641d85dbda1733871b398a3775e8b35e
Elias-Fransson <46138@gymnasium.ax>
2023-01-26 11:02:34 +0200

    fixade github3

--- listsite/settings.py
+++ listsite/settings.py
@@ -14,6 +14,8 @@ SECRET_KEY = 'django-insecure-cx3hk5^l^i_t%pn3k2uputp%cud9!_h0ftn-sto$6=1ovu^p(+
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS =['listsite-production-ef26.up.railway.app']
+
 
 
 
